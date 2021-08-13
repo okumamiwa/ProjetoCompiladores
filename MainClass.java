b755abe54b7c6347d995e230d66487cdd95fe276
okuma.miwa <okuma.miwa@gmail.com>
2021-08-13 19:36:38 -0300

    Updating checklist

--- MainClass.java
+++ MainClass.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 public class MainClass{ 
-  public static void main(String args[]){
-       Scanner _key = new Scanner(System.in);
-double  a;
-double  b;
-double  c;
-double  d;
-double  i;
-String  t1;
-a= _key.nextDouble();
-b= _key.nextDouble();
-a = 1+2*3/b;
-if (a<b) {
-System.out.println(a);}else {
-System.out.println(b);}
-
-t1 = "testing";
-for (i=0;i<3;i=i+1) {
-System.out.println(a);}
-System.out.println(t1);
-}}
+	public static void main(String args[]){
+ 		Scanner _key = new Scanner(System.in);
+		double  a;
+		double  b;
+		double  c;
+		double  d;
+		double  i;
+		String  t1;
+		a= _key.nextDouble();
+		b= _key.nextDouble();
+		a = 1+2*3/b;
+		if (a<b) {
+			System.out.println(a);
+		}
+		else {
+			System.out.println(b);
+		}
+		t1 = "testing";
+		for (i=0;i<3;i=i+1) {
+			System.out.println(a);
+		}
+		System.out.println(t1);
+	}
+}
